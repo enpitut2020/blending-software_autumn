dc1ad3919e919196ff2098beab77a4952385c61c
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-10-23 16:14:57 +0900

    空のコンポーネントファイルを作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <v-container>
+    <originalHeader />
+    <cover />
+    <detail />
+    <history />
     <h1>ホーム画面</h1>
     <router-link to="/list">Go to List</router-link>
   </v-container>
 </template>
+
+<script>
+import originalHeader from "@/components/OriginalHeader.vue";
+import cover from "@/components/Cover.vue";
+import detail from "@/components/Detail.vue";
+import history from "@/components/History.vue";
+
+export default {
+  name: "Home",
+  components: {
+    originalHeader,
+    cover,
+    detail,
+    history
+  }
+};
+</script>
