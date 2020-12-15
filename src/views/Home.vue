4a32332e5638a11f268aefc1c4aa753727090992
name <email>
2020-12-16 03:03:02 +0900

    firestore連携

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,10 @@
 import originalHeader from "@/components/OriginalHeader.vue";
 import cover from "@/components/Cover.vue";
 import detail from "@/components/Detail.vue";
+import getUserRowset from "@/firebase/firestore.js";
+
+let userRowset = getUserRowset()
+console.log(userRowset);
 
 export default {
   name: "Home",
