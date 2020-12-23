6a301033216a343cdcac7ddd8ed5c561404ab0fa
sghirg <suganami@kde.cs.tsukuba.ac.jp>
2020-12-23 12:22:36 +0900

    Add vue-js-modal, vue-turn-js, vue-js-toggle-button

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router';
+import { FlippingWidgets } from "vue-turnjs";
+import "vue-turnjs/dist/vue-turnjs.esm.css";
+import VModal from 'vue-js-modal'
+import ToggleButton from 'vue-js-toggle-button'
 
 Vue.config.productionTip = false
+
+Vue.use(FlippingWidgets);
+Vue.use(VModal)
+Vue.use(ToggleButton)
+
 
 new Vue({
   vuetify,
